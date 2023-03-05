ad1ddc1fc2e548deb6c76dba796a8f9a368937d8
Jiangnh125 <huonggiang12052003@gmail.com>
2023-03-05 22:11:24 +0700

    update

--- DSA01006_HoanViNguoc.cpp
+++ DSA01006_HoanViNguoc.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 
 int n;
-vector<int> a;
-vector<vector<int>> vt;
-
+string a;
+vector<string> vt;
 
 void SinhHoanVi(int n){
-    for (int i = 1; i <= n; i++){
-        a.push_back(i);
-    }
+    a = "";
+    for (int i = 1; i <= n; i++)
+        a += to_string(i);
     while (1){
         // for (int j = 1; j <= n; j++)
         //     cout << a[j];
@@ -29,7 +28,7 @@ void SinhHoanVi(int n){
         int tg = a[i];
         a[i] = a[k];
         a[k] = tg;
-        // doi cho cac phan tu tu a[j+1] -> a[n]
+        // doi cho cac phan tu tu a[j+1] -> a[n-1]
         int l = i + 1;
         int r = n-1;
         while (l < r){
@@ -41,15 +40,21 @@ void SinhHoanVi(int n){
     }
 }
 
-int main (){
+void solve(){
+    cin >> n;
+    //a.resize(n);
+    vt.clear();
+    SinhHoanVi(n);
+    reverse(vt.begin(), vt.end());
+    for (string i : vt) 
+        cout << i << " ";
+}
+
+int main(){
     int t; cin >> t;
     while (t--){
-        cin >> n;
-        SinhHoanVi(n);
-        for (int i = 1; i <= vt.size(); i++){
-            for (auto x = vt[i].end(); x != vt[i].begin(); x--)
-                cout << *x << " ";
-        }
+        solve();
         cout << endl;
     }
+    return 0;
 }
